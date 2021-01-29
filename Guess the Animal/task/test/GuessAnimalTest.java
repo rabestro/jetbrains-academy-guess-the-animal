f65e8f4ec059d281830ed62f1d6e1892f75d714f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-29 19:27:20 +0200

    Added Stage 2 tests

--- Guess the Animal/task/test/GuessAnimalTest.java
+++ Guess the Animal/task/test/GuessAnimalTest.java
@@ -2,69 +2,32 @@ import animals.Main;
 import org.hyperskill.hstest.dynamic.input.DynamicTestingMethod;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
-import org.hyperskill.hstest.testing.TestedProgram;
 
 import java.io.IOException;
-import java.util.HashSet;
 
 public class GuessAnimalTest extends StageTest<String> {
-    private static final int TEST_RANDOM_RESPONSES = 10;
-
     public GuessAnimalTest() {
         super(Main.class);
     }
 
     @DynamicTestingMethod
-    CheckResult testAnimals() throws IOException {
-        return new Scenario("animals").check();
+    CheckResult incorrectStatement() throws IOException {
+        return new Scenario("incorrect-statement").check();
     }
 
     @DynamicTestingMethod
-    CheckResult negativeAnswers() throws IOException {
-        return new Scenario("negative-answers").check();
+    CheckResult correctStatement() throws IOException {
+        return new Scenario("correct-statement").check();
     }
 
     @DynamicTestingMethod
-    CheckResult positiveAnswers() throws IOException {
-        return new Scenario("positive-answers").check();
+    CheckResult completeScenario() throws IOException {
+        return new Scenario("complete-scenario").check();
     }
 
     @DynamicTestingMethod
-    CheckResult unclearAnswers() throws IOException {
-        return new Scenario("unclear-answers").check();
-    }
-
-    @DynamicTestingMethod
-    CheckResult testRandomGoodbye() throws IOException {
-        final var farewell = new HashSet<String>();
-
-        for (int tries = TEST_RANDOM_RESPONSES; tries > 0; tries--) {
-            final var main = new TestedProgram(Main.class);
-            main.start();
-            farewell.add(main.execute("cat\nyes\n"));
-        }
-
-        if (farewell.size() > 1) {
-            return CheckResult.correct();
-        } else {
-            return CheckResult.wrong("You program should use different ways to farewell the user.");
-        }
-    }
-
-    @DynamicTestingMethod
-    CheckResult testRandomYesNoClarification() throws IOException {
-        final var clarification = new HashSet<String>();
-
-        for (int tries = TEST_RANDOM_RESPONSES; tries > 0; tries--) {
-            final var main = new TestedProgram(Main.class);
-            main.start();
-            clarification.add(main.execute("cat\n#\n"));
-        }
-
-        if (clarification.size() > 1) {
-            return CheckResult.correct();
-        } else {
-            return CheckResult.wrong("You program should use different ways to ask clarification question.");
-        }
+    CheckResult factsQuestion() throws IOException {
+        return new Scenario("facts-and-question").check();
     }
 }
+
--- Guess the Animal/task/test/Scenario.java
+++ Guess the Animal/task/test/Scenario.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 
 import static java.text.MessageFormat.format;
 import static java.util.function.Predicate.not;
@@ -23,14 +22,12 @@ public class Scenario {
     Scenario(String name) throws IOException {
         data = new YAMLMapper().readValue(new File("test/" + name + ".data.yaml"), String[][].class);
         script = new YAMLMapper().readValue(new File("test/" + name + ".script.yaml"), String[][].class);
-        System.out.println("Scenario " + name + " is started.");
     }
 
     CheckResult check() {
         for (var values : data) {
             for (var action : script) {
-                final var command = action[0];
-                switch (command) {
+                switch (action[0]) {
                     case "start":
                         main = new TestedProgram(Main.class);
                         output = action.length == 1 ? main.start()
@@ -48,13 +45,10 @@ public class Scenario {
                                 "not contains", not(output::contains),
                                 "file exists", file -> new File(file).exists(),
                                 "file delete", file -> new File(file).delete(),
-                                "find", pattern -> Pattern.compile(pattern).matcher(output).find(),
                                 "matches", output::matches);
 
-                        final var expected = format(action[1], values);
-                        if (validation.get(command).test(expected)) continue;
-                        final var feedback = format(action[2], values) + ". Expected " + command + ": " + expected;
-                        return wrong(feedback);
+                        if (validation.get(action[0]).test(format(action[1], values))) continue;
+                        return wrong(format(action[2], values));
                 }
             }
         }
