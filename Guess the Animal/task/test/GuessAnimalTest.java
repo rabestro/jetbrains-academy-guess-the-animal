5ab4be7ad62a04bca5da16ec24ca686e88e5d6bf
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-30 15:37:06 +0200

    Added tests for stage 4

--- Guess the Animal/task/test/GuessAnimalTest.java
+++ Guess the Animal/task/test/GuessAnimalTest.java
@@ -1,21 +1,48 @@
-import org.hyperskill.hstest.dynamic.DynamicTest;
+import animals.Main;
+import org.hyperskill.hstest.dynamic.input.DynamicTestingMethod;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class GuessAnimalTest extends StageTest<String> {
-    final String[] script = new String[]{
-            "the-first-question",
-            "positive-answers",
-            "negative-answers",
-            "unclear-answers",
-            "guessing-game"
-    };
 
-    @DynamicTest(data = "script")
-    CheckResult runScripts(final String script) throws IOException {
-        return new Scenario(script).check();
+    public GuessAnimalTest() {
+        super(Main.class);
     }
 
+    @DynamicTestingMethod
+    CheckResult theFirstQuestion() throws IOException {
+        return new Scenario("the-first-question").check();
+    }
+
+    @DynamicTestingMethod
+    CheckResult positiveAnswer() throws IOException {
+        return new Scenario("positive-answers").check();
+    }
+
+    @DynamicTestingMethod
+    CheckResult negativeAnswer() throws IOException {
+        return new Scenario("negative-answers").check();
+    }
+
+    @DynamicTestingMethod
+    CheckResult incorrectAnswer() throws IOException {
+        return new Scenario("unclear-answers").check();
+    }
+
+    @DynamicTestingMethod
+    CheckResult testFileFormats() throws IOException {
+        final var result = new Scenario("file-formats").check();
+
+        Stream.of("yaml", "json", "xml")
+                .map("animals."::concat)
+                .map(File::new)
+                .filter(File::exists)
+                .forEach(File::delete);
+
+        return result;
+    }
 }
--- Guess the Animal/task/test/Scenario.java
+++ Guess the Animal/task/test/Scenario.java
@@ -1,3 +1,4 @@
+import animals.Main;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testing.TestedProgram;
@@ -6,7 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 
 import static java.text.MessageFormat.format;
 import static java.util.function.Predicate.not;
@@ -22,24 +22,19 @@ public class Scenario {
     Scenario(String name) throws IOException {
         data = new YAMLMapper().readValue(new File("test/" + name + ".data.yaml"), String[][].class);
         script = new YAMLMapper().readValue(new File("test/" + name + ".script.yaml"), String[][].class);
-        System.out.println("Scenario '" + name + "' is started.");
-        System.out.println();
     }
 
     CheckResult check() {
         for (var values : data) {
             for (var action : script) {
-                final var command = action[0];
-                switch (command) {
+                switch (action[0]) {
                     case "start":
-                        main = new TestedProgram();
-                        output = action.length == 1
-                                ? main.start()
+                        main = new TestedProgram(Main.class);
+                        output = action.length == 1 ? main.start()
                                 : main.start(format(action[1], values).split(" "));
-                        output = output.trim();
                         continue;
                     case "input":
-                        output = main.execute(format(action[1], values)).trim();
+                        output = main.execute(format(action[1], values));
                         continue;
                     case "finish":
                         if (main.isFinished()) continue;
@@ -50,21 +45,10 @@ public class Scenario {
                                 "not contains", not(output::contains),
                                 "file exists", file -> new File(file).exists(),
                                 "file delete", file -> new File(file).delete(),
-                                "find", pattern -> Pattern.compile(pattern).matcher(output).find(),
                                 "matches", output::matches);
 
-                        final var expected = format(action[1], values);
-                        final var test = validation.get(command);
-                        if (test == null) {
-                            return wrong("Unknown command " + command);
-                        }
-                        if (validation.get(command).test(expected)) {
-                            continue;
-                        }
-                        final var feedback = format(action[2], values) + System.lineSeparator()
-                                + "Expected " + command + ": \"" + expected + "\"" + System.lineSeparator()
-                                + "Actual data is: \"" + output + "\".";
-                        return wrong(feedback);
+                        if (validation.get(action[0]).test(format(action[1], values))) continue;
+                        return wrong(format(action[2], values));
                 }
             }
         }
