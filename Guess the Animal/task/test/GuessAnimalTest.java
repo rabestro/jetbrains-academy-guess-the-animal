e8c456ea1e4845d4ac4a323b62b561d73816779a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-30 15:24:16 +0200

    Deleted deprecated methods in GuessAnimalTest

--- Guess the Animal/task/test/GuessAnimalTest.java
+++ Guess the Animal/task/test/GuessAnimalTest.java
@@ -1,34 +1,21 @@
-import org.hyperskill.hstest.dynamic.input.DynamicTestingMethod;
+import org.hyperskill.hstest.dynamic.DynamicTest;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 
 import java.io.IOException;
 
 public class GuessAnimalTest extends StageTest<String> {
+    final String[] script = new String[]{
+            "the-first-question",
+            "positive-answers",
+            "negative-answers",
+            "unclear-answers",
+            "guessing-game"
+    };
 
-    @DynamicTestingMethod
-    CheckResult theFirstQuestion() throws IOException {
-        return new Scenario("the-first-question").check();
-    }
-
-    @DynamicTestingMethod
-    CheckResult positiveAnswer() throws IOException {
-        return new Scenario("positive-answers").check();
-    }
-
-    @DynamicTestingMethod
-    CheckResult negativeAnswer() throws IOException {
-        return new Scenario("negative-answers").check();
-    }
-
-    @DynamicTestingMethod
-    CheckResult incorrectAnswer() throws IOException {
-        return new Scenario("unclear-answers").check();
-    }
-
-    @DynamicTestingMethod
-    CheckResult guessingGame() throws IOException {
-        return new Scenario("guessing-game").check();
+    @DynamicTest(data = "script")
+    CheckResult runScripts(final String script) throws IOException {
+        return new Scenario(script).check();
     }
 
 }
