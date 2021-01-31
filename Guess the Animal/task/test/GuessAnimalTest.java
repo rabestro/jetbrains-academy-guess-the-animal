b5be6bb8bbc18682b7bd2018fe5eea7c95edfe7a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-31 18:25:40 +0200

    Updated tests

--- Guess the Animal/task/test/GuessAnimalTest.java
+++ Guess the Animal/task/test/GuessAnimalTest.java
@@ -5,32 +5,29 @@ import org.hyperskill.hstest.testcase.CheckResult;
 import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
-import java.util.Map;
 import java.util.stream.Stream;
 
 public class GuessAnimalTest extends StageTest<String> {
 
-    @DynamicTest
-    CheckResult testLanguages() throws IOException {
-        final var languages = Map.of("en", "english", "eo", "esperanto");
-        for (var entry: languages.entrySet()) {
-            final var fileName = "animal" + ("en".equals(entry.getKey()) ? "." : "_" + entry.getKey() + ".");
-            final var locale = Locale.getDefault();
-            final var language = System.getProperty("user.language");
-            Locale.setDefault(new Locale(entry.getKey()));
-            System.setProperty("user.language", entry.getKey());
+    String[] languages = new String[]{"en", "eo"};
 
-            deleteFiles(fileName);
-            final var result = new Scenario(entry.getValue()).check();
-            deleteFiles(fileName);
+    @DynamicTest(data = "languages")
+    CheckResult testLanguages(final String language) throws IOException {
+        final var fileName = "animal" + ("en".equals(language) ? "." : "_" + language + ".");
 
-            Locale.setDefault(locale);
-            System.setProperty("user.language", language);
-            if (!result.isCorrect()) {
-                return result;
-            }
-        }
-        return CheckResult.correct();
+        final var locale = Locale.getDefault();
+        final var systemLanguage = System.getProperty("user.language");
+        Locale.setDefault(new Locale(language));
+        System.setProperty("user.language", language);
+
+        deleteFiles(fileName);
+        final var result = new Scenario(language).check();
+        deleteFiles(fileName);
+
+        Locale.setDefault(locale);
+        System.setProperty("user.language", systemLanguage);
+
+        return result;
     }
 
     private void deleteFiles(String fileName) {
